f9ab9726a2b421b57fa4f64e480f139fc955a2f9
Julio Galvan <julio@epazote.net>
2019-06-06 01:51:09 -0700

    refactor auth functions

--- authentication.go
+++ authentication.go
@@ -1,84 +1,64 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "encoding/json"
+	"encoding/json"
+	"fmt"
+	"net/http"
 )
 
 type AuthRequest struct {
-    Auth struct {
-        Mxid string `json:"mxid"`
-        Localpart string `json:"localpart"`
-        Domain string `json:"domain"`
-        Password string `json:"password"`
-    } `json:"auth"`
+	Auth struct {
+		Mxid      string `json:"mxid"`
+		Localpart string `json:"localpart"`
+		Domain    string `json:"domain"`
+		Password  string `json:"password"`
+	} `json:"auth"`
 }
 
 type AuthResponse struct {
-    Auth AuthResponseBody `json:"auth"`
+	Auth AuthResponseBody `json:"auth"`
 }
 
 type AuthResponseBody struct {
-    Success bool `json:"success"`
-    Id *AuthResponseBodyId `json:"id,omitempty"`
-    Profile *AuthResponseBodyProfile `json:"profile,omitempty"`
-}
-
-type AuthResponseBodyId struct {
-    Type string `json:"type,omitempty"`
-    Value string `json:"value,omitempty"`
-}
-
-type AuthResponseBodyProfile struct {
-    DisplayName string `json:"display_name,omitempty"`
-    ThreePIDS   []Profile3PIDS `json:"three_pids,omitempty"`
+	Success bool         `json:"success"`
+	ID      *LookUpID    `json:"id,omitempty"`
+	Profile *ProfileBody `json:"profile,omitempty"`
 }
 
 func Authentication(w http.ResponseWriter, r *http.Request) {
-    if r.Method == "POST" {
-        decoder := json.NewDecoder(r.Body)
-        var req AuthRequest
-        err := decoder.Decode(&req)
-        if err != nil {
-            fmt.Println("Invalid json request")
-            return
-        }
-        res := AuthResponse {
-            Auth: AuthResponseBody {
-                Success: false,
-            },
-        }
-        _, err = loginKeycloak(req.Auth.Localpart, req.Auth.Password)
-        if err != nil {
-            fmt.Println(err)
-        } else {
-            token, err := getKeycloakToken()
-            if err != nil {
-                fmt.Println(err)
-            } else {
-                user, err := getUserArray(token, req.Auth.Localpart)
-                if err != nil {
-                    fmt.Println(err)
-                } else {
-                    res = AuthResponse {
-                        Auth: AuthResponseBody {
-                            Success: true,
-                            Id: &AuthResponseBodyId {
-                                Type: "localpart",
-                                Value: req.Auth.Localpart,
-                            },
-                            Profile: &AuthResponseBodyProfile {
-                                DisplayName: getDisplayName(user),
-                                ThreePIDS: getProfile3PIDS(user),
-                            },
-                        },
-                    }
-                }
-            }
-        }
-        w.Header().Set("Access-Control-Allow-Origin", "*")
-        w.Header().Set("Content-Type", "application/json")
-        json.NewEncoder(w).Encode(res)
-    }
+	if r.Method != "POST" {
+		return
+	}
+	var req AuthRequest
+	if json.NewDecoder(r.Body).Decode(&req) != nil {
+		fmt.Println("Invalid json request")
+		return
+	}
+	res := AuthResponse{
+		Auth: AuthResponseBody{
+			Success: false,
+		},
+	}
+	_, err := loginKeycloak(req.Auth.Localpart, req.Auth.Password)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		profile, err := buildProfile(req.Auth.Localpart)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			res = AuthResponse{
+				Auth: AuthResponseBody{
+					Success: true,
+					ID: &LookUpID{
+						Type:  "localpart",
+						Value: req.Auth.Localpart,
+					},
+					Profile: profile,
+				},
+			}
+		}
+	}
+	prepareResponse(w, res)
+
 }
